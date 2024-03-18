9f85ec2af72242cea77fd43d9174d4457e638df5
Kevin Blanchard <blanchardkevinj@gmail.com>
2024-03-18 08:52:25 -0400

    ems

--- include/GoonEngine/gnpch.h
+++ include/GoonEngine/gnpch.h
@@ -2,7 +2,11 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <time.h>
+#ifdef __EMSCRIPTEN__
+#include <SDL.h>
+#else
 #include <SDL2/SDL.h>
+#endif
 
 #define STB_IMAGE_IMPLEMENTATION
 // #include <SDL2/SDL_image.h>
