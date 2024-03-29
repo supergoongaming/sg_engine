4c9c462567508425a4318cdb93c7c6ff51342cd7
Kevin Blanchard <blanchardkevinj@gmail.com>
2024-03-29 18:51:25 -0400

    get texture working on opengl

--- src/GoonEngine/Texture2D.c
+++ src/GoonEngine/Texture2D.c
@@ -2,8 +2,33 @@
 #include <glad/glad.h>
 #include <png.h>
 #include <GoonEngine/Texture2D.h>
+#include <stb/image.h>
 
-static unsigned char *loadPNG(const char *filename, int *widthp, int *heightp)
+static void flipImageVertically(unsigned char *imageData, int width, int height, int channels)
+{
+    unsigned char *tempRow = (unsigned char *)malloc(width * channels);
+    if (!tempRow)
+    {
+        fprintf(stderr, "Error: Unable to allocate memory for temporary row\n");
+        return;
+    }
+
+    int rowSize = width * channels;
+    for (int y = 0; y < height / 2; y++)
+    {
+        int topRowIndex = y * rowSize;
+        int bottomRowIndex = (height - y - 1) * rowSize;
+
+        // Swap rows
+        memcpy(tempRow, imageData + topRowIndex, rowSize);
+        memcpy(imageData + topRowIndex, imageData + bottomRowIndex, rowSize);
+        memcpy(imageData + bottomRowIndex, tempRow, rowSize);
+    }
+
+    free(tempRow);
+}
+
+static unsigned char *loadPNG(const char *filename, int *widthp, int *heightp, int *channels)
 {
     FILE *file = fopen(filename, "rb");
     if (!file)
@@ -60,74 +85,78 @@ static unsigned char *loadPNG(const char *filename, int *widthp, int *heightp)
     png_byte color_type = png_get_color_type(png_ptr, info_ptr);
     png_byte bit_depth = png_get_bit_depth(png_ptr, info_ptr);
 
-    // Read any color_type to 8-bit depth, RGBA format.
-    // See http://www.libpng.org/pub/png/libpng-manual.txt
-
-    if (bit_depth == 16)
-        png_set_strip_16(png_ptr);
-
-    if (color_type == PNG_COLOR_TYPE_PALETTE)
-        png_set_palette_to_rgb(png_ptr);
-
-    if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
-        png_set_expand_gray_1_2_4_to_8(png_ptr);
-
-    if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
-        png_set_tRNS_to_alpha(png_ptr);
-
-    if (color_type == PNG_COLOR_TYPE_RGB ||
-        color_type == PNG_COLOR_TYPE_GRAY ||
-        color_type == PNG_COLOR_TYPE_PALETTE)
-        png_set_filler(png_ptr, 0xFF, PNG_FILLER_AFTER);
-
-    if (color_type == PNG_COLOR_TYPE_GRAY ||
-        color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
-        png_set_gray_to_rgb(png_ptr);
-
-    // Update the png info struct.
-    png_read_update_info(png_ptr, info_ptr);
+    switch (color_type)
+    {
+    case PNG_COLOR_TYPE_RGBA:
+        *channels = 4;
+        break;
+    case PNG_COLOR_TYPE_RGB:
+        *channels = 3;
+        break;
+    case PNG_COLOR_TYPE_GRAY_ALPHA:
+        *channels = 2;
+        break;
+    case PNG_COLOR_TYPE_GRAY:
+    case PNG_COLOR_TYPE_PALETTE:
+        *channels = 1;
+        break;
+    default:
+        // Unsupported color type
+        fclose(file);
+        png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+        fprintf(stderr, "Error: Unsupported color type\n");
+        return NULL;
+    }
 
     // Allocate memory for image data
-    png_bytep *row_pointers = (png_bytep *)malloc(sizeof(png_bytep) * height);
-    for (int y = 0; y < height; y++)
+    unsigned char *imageData = (unsigned char *)malloc(width * height * 4);
+    if (!imageData)
     {
-        row_pointers[y] = (png_byte *)malloc(png_get_rowbytes(png_ptr, info_ptr));
+        fclose(file);
+        png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+        fprintf(stderr, "Error: Unable to allocate memory for image data\n");
+        return NULL;
     }
 
     // Read the image data
+    png_bytep row_pointers[height];
+    for (int y = 0; y < height; y++)
+    {
+        row_pointers[y] = (png_byte *)malloc(png_get_rowbytes(png_ptr, info_ptr));
+        if (!row_pointers[y])
+        {
+            // Handle memory allocation failure
+            fclose(file);
+            png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+            fprintf(stderr, "Error: Unable to allocate memory for row pointers\n");
+            free(imageData);
+            return NULL;
+        }
+    }
+
+    // Read the image row by row
     png_read_image(png_ptr, row_pointers);
-    unsigned char *imageData = (unsigned char *)malloc(width * height * 4);
 
     // Copy image data from row_pointers to imageData in RGBA format
     for (int y = 0; y < height; y++)
     {
+        png_bytep row = row_pointers[y];
         for (int x = 0; x < width; x++)
         {
-            // Each pixel in row_pointers consists of RGB (and optionally alpha) channels
-            // You need to convert it to RGBA format
-            int srcIndex = y * width * 3 + x * 3;  // Index in row_pointers
-            int destIndex = y * width * 4 + x * 4; // Index in imageData
-
-            // Copy RGB channels
-            imageData[destIndex] = row_pointers[y][srcIndex];         // Red
-            imageData[destIndex + 1] = row_pointers[y][srcIndex + 1]; // Green
-            imageData[destIndex + 2] = row_pointers[y][srcIndex + 2]; // Blue
-
-            // Set alpha channel to 255 (fully opaque)
-            imageData[destIndex + 3] = 255;
+            png_bytep px = &(row[x * 4]);
+            int destIndex = (y * width + x) * 4;
+            imageData[destIndex] = px[0];     // Red
+            imageData[destIndex + 1] = px[1]; // Green
+            imageData[destIndex + 2] = px[2]; // Blue
+            imageData[destIndex + 3] = px[3]; // Alpha
         }
-    }
-
-    // // Free libpng allocated memory
-    for (int y = 0; y < height; y++)
-    {
         free(row_pointers[y]);
     }
-    free(row_pointers);
 
-    // // Clean up libpng structures
+    // Clean up libpng structures
     png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
     fclose(file);
+
     // Set returns
     *widthp = width;
     *heightp = height;
@@ -137,11 +166,18 @@ static unsigned char *loadPNG(const char *filename, int *widthp, int *heightp)
 geTexture2D *geTexture2DNew()
 {
     geTexture2D *texture = calloc(1, sizeof(*texture));
-    texture->Internal_Format = GL_RGB;
+    // Only using pngs for now, so always set RGBA
+    texture->Internal_Format = GL_RGBA;
+    texture->Image_Format = GL_RGBA;
     texture->Wrap_S = GL_REPEAT;
-    texture->Filter_Min = GL_LINEAR;
-    texture->Filter_Max = GL_LINEAR;
+    texture->Wrap_T = GL_REPEAT;
+    // texture->Filter_Min = GL_LINEAR;
+    // texture->Filter_Max = GL_LINEAR;
+    // We want it to look classic, so use nearest and not linear
+    texture->Filter_Min = GL_NEAREST;
+    texture->Filter_Max = GL_NEAREST;
     glGenTextures(1, &texture->ID);
+    return texture;
 }
 
 void geTexture2DFree(geTexture2D *texture)
@@ -153,11 +189,17 @@ void geTexture2DGenerate(geTexture2D *texture, const char *filename)
 {
     int width = 0;
     int height = 0;
-    unsigned char *data = loadPNG(filename, &width, &height);
+    int channels = 0;
+    unsigned char *data = loadPNG(filename, &width, &height, &channels);
+    flipImageVertically(data, width, height, channels);
+    // If we want to test with stb.
+    // stbi_set_flip_vertically_on_load(1);
+    // unsigned char *data = stbi_load(filename, &width, &height, &channels, 0);
     texture->Width = width;
     texture->Height = height;
     glBindTexture(GL_TEXTURE_2D, texture->ID);
     glTexImage2D(GL_TEXTURE_2D, 0, texture->Internal_Format, width, height, 0, texture->Image_Format, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, texture->Wrap_S);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, texture->Wrap_T);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, texture->Filter_Min);
